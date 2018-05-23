a6430d50e4fdc581463043f5546b3104855a0b95
Jan Scheufler <werewolf22@t-online.de>
2018-05-23 09:30:47 +0200

    spec - spec_helper_acceptance - removed surplus commands

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -13,16 +13,9 @@ RSpec.configure do |c|
   c.before :suite do
     # Install module to all hosts
     hosts.each do |host|
-
       install_puppet_on(host)
 
-      result = on(host, "puppet config print modulepath")
-      modulepaths = result.stdout.split(":")
-      modulepath = modulepaths[modulepaths.length - 1]
-      logger.info('modulepath: ' + modulepath)
-
-      install_dev_puppet_module_on(host, :source => module_root, :module_name => 'secc_snmpd',
-          :target_module_path => modulepath)
+      install_dev_puppet_module_on(host, :source => module_root, :module_name => 'secc_snmpd',)
       # Install dependencies
       on(host, puppet('module', 'install', 'puppetlabs-stdlib'))
       on(host, puppet('module', 'install', 'puppetlabs-concat'))
