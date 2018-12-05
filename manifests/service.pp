598cc4228eb4475789ac0247d4d27bd44bad1bf2
vmpr <github@it-dd.net>
2018-12-05 17:17:36 +0100

    removed comments

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,15 +1,5 @@
 class secc_snmpd::service {
 
-#  if $::secc_snmpd::v2_enabled or $::secc_snmpd::v3_enabled {
-#    service { 'snmpd':
-#      ensure     => running,
-#      hasrestart => true,
-#      hasstatus  => true,
-#      enable     => true,
-#      require    => Class['secc_snmpd::install'],
-#    }
-#  }
-
   if $::secc_snmpd::v2_enabled or $::secc_snmpd::v3_enabled {
     if $::operatingsystem == 'XCP' {
       service { 'snmpd':
