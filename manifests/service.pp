d4b25016e6ea46d5b839606c39752e7e712f42c4
vmpr <github@it-dd.net>
2018-12-06 01:11:29 +0800

    added compatibility for xcp-ng to secc_snmpd puppet module (#13)
    * added XCP as valid operatingsystem
      XCP is a Citrix XenServer Fork by Vates
    
    * added compatibility for xcp-ng to install and service manifest

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,12 +1,24 @@
 class secc_snmpd::service {
 
   if $::secc_snmpd::v2_enabled or $::secc_snmpd::v3_enabled {
-    service { 'snmpd':
-      ensure     => running,
-      hasrestart => true,
-      hasstatus  => true,
-      enable     => true,
-      require    => Class['secc_snmpd::install'],
+    if $::operatingsystem == 'XCP' {
+      service { 'snmpd':
+        ensure     => running,
+        hasrestart => true,
+        hasstatus  => true,
+        enable     => true,
+        require    => Class['secc_snmpd::install'],
+        provider   => systemd
+      }
+    }
+    else {
+      service { 'snmpd':
+        ensure     => running,
+        hasrestart => true,
+        hasstatus  => true,
+        enable     => true,
+        require    => Class['secc_snmpd::install'],
+      }
     }
   }
 
